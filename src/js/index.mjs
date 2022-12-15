36db3e5614172fa98982fc0def02bb003d32c4ca
bushrakalaji <91549377+bushrakalaji@users.noreply.github.com>
2022-12-15 23:49:30 +0100

    som changes

---
+++ src/js/index.mjs
@@ -0,0 +1,3 @@
+export const API_HOST_URL = "https://api.noroff.dev/api/v1"
+export const API_AUCTION_BASE = "/auction/listings"
+export const API_AUCTION_URL = `${API_AUCTION_URL}${API_AUCTION_BASE}`;
